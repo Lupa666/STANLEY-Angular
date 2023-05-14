c919db361563f313a895e19f2b1b1e322b5698d4
Lupa666 <bartkowski_so@interia.pl>
2023-05-14 13:19:13 +0200

    lol

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `<p>
+    ALOOOOOOOHA!
+  </p>`,
+  styles: ['p{color: #125699}']
+})
+export class TestComponent {
+
+}
