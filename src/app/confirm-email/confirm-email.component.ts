ebc94ab8442650267e07f3bbd6497ad5ca3daec8
Lupa666 <bartkowski_so@interia.pl>
2023-05-30 00:01:21 +0200

    Included logging in, expanded header, added route guards

---
+++ src/app/confirm-email/confirm-email.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-email',
+  template: `
+    <p>
+      Wysłano wiadomość na twojego maila! Potwierdź ją i zaloguj się.
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ConfirmEmailComponent {
+
+}
